74b398249cd2c5ec670002dd24a2fd88e9226cb7
akeeb12 <112759560+akeeb12@users.noreply.github.com>
2024-09-27 08:21:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,3 +6,24 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+
+function togglePlay() {
+	const method = video.paused ? 'play' : 'pause';
+	video[method] ();
+}
+function updateButton() {
+	const icon = this.paused ? '►' : '❚❚';
+	toggle.textContent = icon;
+}
+function handleRangeUpdate(params) {
+	video[this.name] = this.value;
+}
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('click', togglePlay);
+
+
+
